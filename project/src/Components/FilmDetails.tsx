5e5c93cef3793ac3292e01aaf4454df4fdeea9a3
mansuy maxime <mmansuy@norauto.fr>
2022-03-16 13:47:40 +0100

    maj

--- project/src/Components/FilmDetails.tsx
+++ project/src/Components/FilmDetails.tsx
@@ -4,11 +4,38 @@ import React, { useEffect, useState } from 'react';
 
 function FilmsDetails(props:any) {
 
+  const [film, setFilm] = useState(<></>);
 
+  useEffect(() => {
+    async function fetchMyAPI() {
+      const response = await fetch(`https://api.themoviedb.org/3/movie/${props.idFilmSelect}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`)
+      const responseFormat = await response.json();
+      setFilm(
+        <div className='container'>
+          <div className="card mb-3">
+          <div className="row no-gutters">
+            <div className="col-md-4">
+              <img src={`https://image.tmdb.org/t/p/w500/${responseFormat.poster_path}`} className="card-img" alt="..." />
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                  <h5 className="card-title">{responseFormat.title}</h5>
+                  <p className="card-text">{responseFormat.overview}</p>
+                  <p className="card-text"><small className="text-muted">Budget: {responseFormat.budget} $</small></p>
+                  <p className="card-text"><small className="text-muted">Product contry : {responseFormat.production_countries[0].name}</small></p>
+                  <p className="card-text"><small className="text-muted">Release date : {responseFormat.release_date}</small></p>
+              </div>
+            </div>
+          </div>
+          </div>
+        </div>)
+    }
+    fetchMyAPI()
+  }, []);
 
   return (
     <div className="container">
-coucou
+      {film}
     </div>
 
 
--- project/src/Components/FilmsPerGenres.tsx
+++ project/src/Components/FilmsPerGenres.tsx
@@ -7,14 +7,13 @@ function FilmsPerGenres(props:any) {
   const [filmDetailLoad, setFilmDetailLoad] = useState("");
   const [filmDetail,setFilmDetail] = useState(<></>);
 
-  function loadFilmDetails(event: any) {
-    setFilmDetailLoad(event.target.dataset.value)
-    setFilmDetail(< FilmDetails />);
+  function loadFilmDetails(id: any) {
+    setFilmDetailLoad(id)
+    setFilmDetail(< FilmDetails idFilmSelect={id}/>);
   }
-  console.log(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${props.page}&with_genres=${props.genreSelect}&with_watch_monetization_types=flatrate`)
   useEffect(() => {
     async function fetchMyAPI() {
-      const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${props.page}&with_genres=${props.genreSelect}&with_watch_monetization_types=flatrate`)
+      const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&page=${props.page}&with_genres=${props.genreSelect}&with_watch_monetization_types=flatrate`)
       const responseFormat = await response.json()
       setFilms(responseFormat.results)
     }
@@ -28,12 +27,11 @@ function FilmsPerGenres(props:any) {
         <div className="row">
           {films.map((film: any) => {
             return (
-              <div className="col-2 " >
+              <div className="col-2" data-value={film.id} onClick={()=>(loadFilmDetails(film.id))}  >
                 <div className="card">
                   <img src={`https://image.tmdb.org/t/p/w500/${film.poster_path}`} className="card-img-top" />
                   <div className="card-body">
                     <h5 className="card-title cardFormat">{film.title}</h5>
-                    <a onClick={(loadFilmDetails)} data-value={film.id} className="btn btn-primary">View Info</a>
                   </div>
                 </div>
               </div>)
